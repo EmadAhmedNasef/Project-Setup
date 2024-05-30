e0ba1bfd72f8cb0a3fc93f4deec8c5390efb3f8d
EmadAhmedNasef <emadnasef86@gmail.com>
2024-05-30 18:19:23 +0300

    fixed environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://expensetrackerapis.azurewebsites.net/api' 
+    apiUrl: 'https://expensetrackerapis.azurewebsites.net/api' 
   };
